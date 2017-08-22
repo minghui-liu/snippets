5c089fc220b2bdc5f233c5745adf8737dd1f40e7
Minghui Liu <minghui.kevin.liu@gmail.com>
2017-08-22 16:54:17 -0400

    add vundle setup script

---
+++ setup_vundle.sh
@@ -0,0 +1,6 @@
+#~/bin/sh
+
+# make sure you have vim version >7.3 and you have +python in features
+# run vim --version to check
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
